d8803eb18b3d638010ed04bc6d1d42f4e215a0d5
ZacznijProgramowac <zacznijprogramowac@gmail.com>
2020-03-18 16:46:37 +0100

    Add examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { SelectedDog } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  onStringEvent($event: string) {
+    console.log('string', $event);
+  }
+
+  onBooleanEvent($event: boolean) {
+    console.log('boolean', $event);
+  }
+
+  onNumberEvent($event: number) {
+    console.log('number', $event);
+  }
+
+  onVoidEvent() {
+    console.log('Void event');
+  }
+
+  onDateEvent($event: Date) {
+    console.log('Date', $event);
+  }
+
+  onObjectEvent($event: SelectedDog) {
+    console.log('SelectedDog', $event);
+  }
+}
